2f1654d64759bb44fc1c02fd59b85f257e8f18c4
pashkov-dmitriy <pashkov.dmitriy2000@gmail.com>
2026-02-11 14:51:36 +0300

    init commit

---
+++ Domain/Entities/Actor/Actor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG.Domain.Entities
+{
+    internal class Actor
+    {
+    }
+}
---
+++ Domain/Entities/Inventory/IInventory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG.Domain.Inventory
+{
+    public interface IInventory
+    {
+
+    }
+}
